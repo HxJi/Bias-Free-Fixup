6c353e1b95838ea38f152136ad3b8ae1819cfc8d
hj14 <hj14@illinois.edu>
2019-07-27 22:31:16 -0500

    distributed running script

--- imagenet/RN50_FP32_100E.sh
+++ imagenet/RN50_FP32_100E.sh
@@ -1,2 +1,7 @@
-
+# Train on 1 Node with 1 GPU 
 python imagenet_train.py  -a fixup_resnet50 --resume /home/hj14/Bias-Free-Fixup/imagenet/checkpoint-16.pth.tar --epochs 100 -b 64 --gpu 0  [path to imagenet]
+# Train on 1 Node with N GPU
+python imagenet_train.py  -a fixup_resnet50 --resume /home/hj14/Bias-Free-Fixup/imagenet/checkpoint-16.pth.tar --epochs 100 -b 64 [path to imagenet]
+# Train on N Node with M GPU each
+python imagenet_train.py  -a fixup_resnet50 --rank 0 --dist-url tcp://127.0.0.1:23456 --world-size N --epochs 100 -b 64  --multiprocessing-distributed [path to imagenet]
+python imagenet_train.py  -a fixup_resnet50 --rank 1 --dist-url tcp://127.0.0.1:23456 --world-size N --epochs 100 -b 64  --multiprocessing-distributed [path to imagenet]
